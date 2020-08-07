d66ca5865486de0e66f2b6b7de66de7a5d0a16be
Lucas de Queiroz <lucasqreis@gmail.com>
2020-08-07 20:06:57 -0300

    Class 2 over

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,11 +1,15 @@
 import express, { request, response } from 'express';
-import ClassesController from './controllers/ClassesControler';
+import ClassesController from './controllers/ClassesController';
+import ConnectionsController from './controllers/ConnectionsController';
 
 const routes = express.Router();
 const classesControlers = new ClassesController();
+const connectionsController = new ConnectionsController();
 
 routes.get('/classes', classesControlers.index);
 routes.post('/classes' , classesControlers.create);
+
+routes.post('connections', connectionsController.create);
 
 
 export default routes;
